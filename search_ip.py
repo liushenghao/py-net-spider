917521b2e29b7583ca06fe1479afc35060e31a15
liushenghao <1042686202@qq.com>
2019-03-11 21:47:44 +0800

    直接搜索span

--- search_ip.py
+++ search_ip.py
@@ -3,14 +3,14 @@ from bs4 import BeautifulSoup
 url1="http://ip.tool.chinaz.com/"
 url2="http://www.ip138.com/"
 net = 'www.baidu.com'#input('输入网站')
-try:
-    r=requests.get(url1+net)
-    r.raise_for_status()
-    webcode = r.text
-    soup = BeautifulSoup(webcode,'html.parser')
-    for p in soup.p.next_siblings:
-        for span in soup.p.children:
-            print(span) 
+#try:
+r=requests.get(url1+net)
+#r.raise_for_status()
+webcode = r.text
+soup = BeautifulSoup(webcode,'html.parser')
+for link in soup.find_all('span'):
+    if link.attrs.values() == "Whwtdhalf w15-0":
+        print(link.string)
     #提取其中的IP地址net2（使用bs4）
 #     net2='123'
 #     p=requests.get(url2+net2)
